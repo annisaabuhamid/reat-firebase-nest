aebf86da4947609fde94b1d78c97e233cdfa5615
annisaabuhamid <siti.nurannisa@cleargo.com>
2022-09-17 09:48:50 +0800

    1. add styling using MUI

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,50 +1,68 @@
-import { Box,List,ListItem,ListItemButton,ListItemIcon,ListItemText } from '@mui/material';
 
+import '../styles/LandingPage.css';
 import {useState} from 'react';
-import { useNavigate } from "react-router-dom";
-import { routeNames } from '../routes/routeNames';
-import '../styles/App.css';
+import { Button,TextField,List,ListItem,ListItemButton,ListItemText } from  '@mui/material';
+
 
 
 
 const LandingPage = () => {
-  let navigate = useNavigate();
 
-  const [name, setName ] = useState('hi');
-  const updateNameState = (element) =>{
-    const value = element.target.value;
-    setName(value);
+  const [todolist, setTodolist ] = useState([]);
+  const [todo,setTodo] = useState('test');
+  const saveTodo = () =>{
+        //deep clone cause xleh ubah directly todolist
+        const cloneArray = [...todolist] //[...] spread operator
+        cloneArray.push(todo)
+        setTodolist(cloneArray)
+        setTodo("")
   }
-  const [desc, setDesc ] = useState('desc');
-  const updateDescState = (element) =>{
-    const value = element.target.value;
-    setDesc(value);
-  }
-  const navigateToHome = () =>
-  navigate(routeNames.HOME,{state:{
-    firstName:name,
-    desc:desc
-  }})
+  
   return (
-    <div className='container'>
-
-      <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-         <List>
-            <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-              </ListItemIcon>
-              <ListItemText primary="Inbox" />
-            </ListItemButton>
+   <div className='container'>
+    <div>
+    <TextField 
+      onChange={(et) => setTodo(et.target.value)}
+      id="outlined-basic"
+      label="Todo"
+      variant="outlined"
+      value={todo}
+      sx={{
+        width:400,
+      }} />
+      <Button
+      onClick={saveTodo}
+      variant="contained"
+      size="large"
+      sx={{
+        height: 55,
+        marginLeft: 2
+      }}
+      >SAVE
+      </Button>
+      </div>
+      <div>
+         
+      <List>
+        {
+           todolist.map((eachTodo,key)=>(
+          <ListItem disablePadding key={key}>
+           <ListItemButton>
+             <ListItemText primary={eachTodo} />
+           </ListItemButton>
           </ListItem>
-        </List>
-      </Box>
-      <input type="text" id="name"  onChange={updateNameState} />
-      <input type="text" id="desc"  onChange={updateDescState} />
-      { /* add a way to route */}
-      <button onClick={navigateToHome}>Home</button>
-    </div>
+          )
+          )
+        }
+         
+      </List>
+         
+      </div>
+   </div>
+ 
+    
   );
 }
+ 
 
 export default LandingPage;
