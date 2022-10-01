a0801b004d9420ab26c39e1b334528fa9d79af15
annisaabuhamid <siti.nurannisa@cleargo.com>
2022-10-01 11:38:07 +0800

    1. add  a theme

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import {
-  Outlet,
-} from "react-router-dom";
+import { Outlet} from "react-router-dom";
 import NavBar from "./components/NavBar";
+import { createTheme,ThemeProvider } from '@mui/material/styles';
+import { lightGreen } from "@mui/material/colors";
 
 
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main:lightGreen[500],
+    },
+    secondary: {
+      main: "#64dd17",
+    },
+  },
+});
 const App = () => {
 
   return (
+    <ThemeProvider theme={theme}>
     <div>
      <NavBar/>
       <main>
         <Outlet />
         </main>
     </div>
+    </ThemeProvider>
   )
 }
 
